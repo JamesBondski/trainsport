61493a37277889f4553e68da35f0c3b432e51918
Matthias Schröder <aldart@gmail.com>
2018-02-01 17:42:57 +0100

    Projektdateien hinzufügen.

---
+++ Trainsport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trainsport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
